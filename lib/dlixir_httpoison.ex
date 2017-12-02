c5c85e80218167eeb14f1b5d1c2d21d93adfa4fb
Yoshikazu Aoshima <yaoshimax@gmail.com>
2017-12-03 05:08:28 +0900

    call HTTPoison.stream_next after receiving HTTPoison.AsyncStatus

--- lib/dlixir_httpoison.ex
+++ lib/dlixir_httpoison.ex
@@ -20,13 +20,10 @@ defmodule Dlixir.HTTPoison do
   end
 
   def message_receive(file_io, buffer, opts, resp) do
-    if Keyword.get(opts, :once) do
-      {:ok, ^resp} = HTTPoison.stream_next(resp)
-    end
     buffersize = Keyword.get(opts, :buffersize)
     receive do
-      %HTTPoison.AsyncStatus{code: 200} -> message_receive(file_io, buffer, opts, resp)
-      %HTTPoison.AsyncHeaders{} -> message_receive(file_io, buffer, opts, resp)
+      %HTTPoison.AsyncStatus{code: 200} -> next_message_receive(file_io, buffer, opts, resp)
+      %HTTPoison.AsyncHeaders{} -> next_message_receive(file_io, buffer, opts, resp)
       %HTTPoison.AsyncChunk{chunk: chunk} -> 
         buffer = 
           case byte_size(chunk) + byte_size(buffer) > buffersize do
@@ -36,12 +33,19 @@ defmodule Dlixir.HTTPoison do
             false -> 
               buffer
           end
-        message_receive(file_io, buffer <> chunk, opts, resp)
+        next_message_receive(file_io, buffer <> chunk, opts, resp)
       %HTTPoison.AsyncEnd{id: _id} ->
         IO.binwrite(file_io, buffer)
         IO.write("finished!")
       invalid_message -> 
         :io.format "invalid_messagee ~p", [invalid_message]
     end
+  end
+
+  def next_message_receive(file_io, buffer, opts, resp) do
+    if Keyword.get(opts, :once) do
+      {:ok, ^resp} = HTTPoison.stream_next(resp)
+    end
+    message_receive(file_io, buffer, opts, resp)
   end
 end
